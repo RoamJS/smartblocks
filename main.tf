11d4a772690f6108bd3f9c69015e591458d03300
David Vargas <dvargas92495@gmail.com>
2021-07-27 13:57:33 -0700

    name index on store

--- main.tf
+++ main.tf
@@ -84,9 +84,23 @@ resource "aws_dynamodb_table" "store" {
     type = "S"
   }
 
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
   global_secondary_index {
     hash_key           = "status"
     name               = "status-index"
+    non_key_attributes = []
+    projection_type    = "ALL"
+    read_capacity      = 0
+    write_capacity     = 0
+  }
+
+  global_secondary_index {
+    hash_key           = "name"
+    name               = "name-index"
     non_key_attributes = []
     projection_type    = "ALL"
     read_capacity      = 0
