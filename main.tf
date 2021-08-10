4b929fce99edc0d7bb2805c1b4efb25b95a165fa
David Vargas <dvargas92495@gmail.com>
2021-08-09 21:01:05 -0500

    add status author index

--- main.tf
+++ main.tf
@@ -119,6 +119,16 @@ resource "aws_dynamodb_table" "store" {
     write_capacity     = 0
   }
 
+  global_secondary_index {
+    hash_key           = "author"
+    name               = "status-author-index"
+    non_key_attributes = []
+    projection_type    = "ALL"
+    range_key          = "status"
+    read_capacity      = 0
+    write_capacity     = 0
+  }
+
   tags = {
     Application = "Roam JS Extensions"
   }
